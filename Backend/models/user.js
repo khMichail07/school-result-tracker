49aa4068b03031639a9f65ade5b186c88dc79aca
khMichail07 <kobbyhammond102@gmail.com>
2026-09-21 23:39:12 +0000

    Fix backend for deployment

--- Backend/models/user.js
+++ Backend/models/user.js
@@ -59,6 +59,6 @@ userSchema.methods.toJSON = function () {
   return user;
 };
 
-const User = mongoose.model('User', userSchema);
+const User = mongoose.models.User || mongoose.model('User', userSchema);
 module.exports = User;
 
